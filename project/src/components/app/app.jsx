4370d5881d9c767bcbab3634771cab8f72f1d3a3
Игорь Славнов <i.slavnov@andersenlab.com>
2021-06-05 02:16:52 +0300

    Добавляет компоненты

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import HomePage from '../home-page/home-page';
 
-function App() {
-  return <p>Hello, world!</p>;
+function App({ cardsCount, rentOffers }) {
+  return (
+    <HomePage
+      cardsCount={cardsCount}
+      rentOffers={rentOffers}
+    />
+  );
 }
+
+App.propTypes = {
+  cardsCount: PropTypes.number.isRequired,
+  rentOffers: PropTypes.number.isRequired,
+};
 
 export default App;
--- project/src/index.js
+++ project/src/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Setting = {
+  CARDS_COUNT: 5,
+  RENT_OFFERS: 312,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      cardsCount={Setting.CARDS_COUNT}
+      rentOffers={Setting.RENT_OFFERS}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
