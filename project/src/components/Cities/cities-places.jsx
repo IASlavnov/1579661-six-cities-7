c7b0920630c93e26204113500341061cbd4e8099
Игорь Славнов <i.slavnov@andersenlab.com>
2021-07-17 22:38:15 +0300

    Исправляет сортировку

--- project/src/components/Cities/cities-places.jsx
+++ project/src/components/Cities/cities-places.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { ActionCreator } from '../../store/action';
@@ -8,30 +8,24 @@ import offersPropTypes from './offers.prop';
 import { PlaceType } from '../../const';
 import { setSort } from '../../utils/sort';
 
-function CitiesPlaces({ filteredOffers, city, sortType, setActiveCard }) {
-  const offersForSort = filteredOffers.slice();
-
-  useEffect(() => {
-    setSort(offersForSort, sortType);
-  }, [offersForSort, filteredOffers, sortType]);
-
-  const cards = offersForSort.map((offer) => (
-    <Card
-      key={offer.id}
-      offer={offer}
-      onMouseEnter={() => setActiveCard(offer.id)}
-      onMouseLeave={() => setActiveCard()}
-      placeType={PlaceType.MAIN}
-    />
-  ));
-
+function CitiesPlaces({ filteredOffers, city, setActiveCard }) {
   return (
     <section className="cities__places places">
       <h2 className="visually-hidden">Places</h2>
-      <b className="places__found">{offersForSort.length} places to stay in {city}</b>
+      <b className="places__found">{filteredOffers.length} places to stay in {city}</b>
       <SortForm />
       <div className="cities__places-list places__list tabs__content">
-        {cards}
+        {
+          filteredOffers.map((offer) => (
+            <Card
+              key={offer.id}
+              offer={offer}
+              onMouseEnter={() => setActiveCard(offer.id)}
+              onMouseLeave={() => setActiveCard()}
+              placeType={PlaceType.MAIN}
+            />
+          ))
+        }
       </div>
     </section>
   );
@@ -40,13 +34,12 @@ function CitiesPlaces({ filteredOffers, city, sortType, setActiveCard }) {
 CitiesPlaces.propTypes = {
   filteredOffers: offersPropTypes,
   city: PropTypes.string.isRequired,
-  sortType: PropTypes.string.isRequired,
   setActiveCard: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = ({ city, filteredOffers, sortType }) => ({
   city,
-  filteredOffers,
+  filteredOffers: setSort(filteredOffers.slice(), sortType),
   sortType,
 });
 
